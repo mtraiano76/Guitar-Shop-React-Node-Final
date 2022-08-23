27d094775d960e65a1e8f3aedc87dcc4e39ad589
Miguel Traiano <92335885+mtraiano76@users.noreply.github.com>
2022-08-23 09:51:49 -0300

    Contacto

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var novedadesModel = require ("./../models/novedadesModel")
-const cloudinary = require('cloudinary').v2;
+var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 
 router.get('/novedades', async function(req, res, next) {
   let novedades = await novedadesModel.getNovedades();
@@ -27,5 +28,32 @@ router.get('/novedades', async function(req, res, next) {
 
   res.json(novedades);
 });
+
+
+router.post("/contacto", async (req,res)=>{
+  const mail = {
+    to: "mtraiano76@gmail.com",
+    subject: "Contacto Web",
+    html: `${req.body.nombre} se contacto a traves de la web y quiere mas informacion a este correo: ${req.body.email} <br> Ademas, hizo el siguiente comentario: ${req.body.mensaje} <br> Su tel es: ${req.body.telefono}`
+  }
+
+  const transport = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+    }
+  });
+
+  await transport.sendMail(mail)
+
+  res.status(201).json({
+    error:false,
+    message: "Mensaje Enviado"
+  });
+
+});
+
 
 module.exports = router;
--- frontend/src/Pages/ContactoPage.jsx
+++ frontend/src/Pages/ContactoPage.jsx
@@ -1,24 +1,81 @@
+import "./styles/ContactoPage.css"
+import { useState } from "react";
+import axios from "axios";
+
 const ContactoPage = (props) => {
-    return(
-    
-        <div className="">
-        <h2>Fromulario de Contacto</h2>
-          <form>
-              <label>
-                  <input type="text" name="name" placeholder="Ingrese su Nombre"></input>
-              </label>
-              <label>
-                  <input type="text" name="name" placeholder="Ingrese su Apellido"></input>
-              </label>
-              <label>
-                  <input type="text" name="name" placeholder="Ingrese su Email"></input>
-              </label>
-              <textarea name="textarea" rows="10" cols="100">Envienos su mensaje y nos contactaremos a la brevedad</textarea>
-              <label>
-              <input class="button" type="submit" value="Enviar"></input>
-              </label>
-          </form>
-      </div>
-    );
-}
-export default ContactoPage;
+
+    const initialForm = {
+        nombre: "",
+        email: "",
+        telefono: "",
+        mensaje: ""
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState("");
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg("");
+        setSending(true)
+        const response = await axios.post("http://localhost:3000/api/contacto", formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialForm)
+        }
+    }
+
+
+    return (
+        <main className="holder contacto">
+            <div>
+                <h2>Contacto Rapido</h2>
+                <form action="/contacto" method="post" onSubmit={handleSubmit} className="formulario">
+                    <p>
+                        <label for="nombre">Nombre</label>
+                        <input type="text" name="nombre" value={formData.nombre} onChange={handleChange}/>
+                    </p>
+                    <p>
+                        <label for="email">Email</label>
+                        <input type="text" name="email" value={formData.email} onChange={handleChange}/>
+                    </p>
+                    <p>
+                        <label for="telefono">Telefono</label>
+                        <input type="text" name="telefono" value={formData.telefono} onChange={handleChange}/>
+                    </p>
+                    <p>
+                        <label for="mensaje">Mensaje</label>
+                        <textarea name="mensaje" value={formData.mensaje} onChange={handleChange}></textarea>
+                    </p>
+                    <p className="acciones"><input type="submit" value="Enviar" /></p>
+                
+                </form>
+                {sending ? <p>Enviando...</p> : null}
+                {msg ? <p>{msg}</p> : null}
+
+
+            </div>
+            <div className="datos">
+                <h2>Otras vias de comunicacion</h2>
+                <p>Tambien puede contactarse con nosotros usando los siguientes medios</p>
+                <ul>
+                    <li>Telefono: 12345678</li>
+                    <li>Email: contacto@guitarshop.com</li>
+                    <li>Facebook:</li>
+                    <li>Instagram:</li>
+                </ul>
+            </div>
+        </main>
+    )
+    }
+            export default ContactoPage;
